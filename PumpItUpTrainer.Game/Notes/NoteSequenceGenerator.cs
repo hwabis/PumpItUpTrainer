1c90eac100158f211d4e5518d5e7ec81aa5695fe
hwabis <kennysun23@gmail.com>
2024-07-18 23:24:32 -0400

    ಥ_ಥ

--- PumpItUpTrainer.Game/Notes/NoteSequenceGenerator.cs
+++ PumpItUpTrainer.Game/Notes/NoteSequenceGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace PumpItUpTrainer.Game.Notes
 {
@@ -48,15 +49,15 @@ namespace PumpItUpTrainer.Game.Notes
         }
 
         // Banned patterns:
-        // 1. [Right foot] DL C UL
-        // 2. [Right foot] UL C DL
-        // 3. [Left foot] DR C UR
-        // 4. [Left foot] UR C DR
+        // [RLR] DL C UL
+        // [RLR] UL C DL
+        // [LRL] DR C UR
+        // [LRL] UR C DR
         public static List<Note> GenerateNoteSequence(int noteCount, Foot startingFoot, List<Note> allowedNotes, bool repeatsAllowed)
         {
             if (noteCount < 1)
             {
-                throw new Exception("🤔");
+                return new();
             }
 
             List<Note> generatedNotes = new();
@@ -78,23 +79,91 @@ namespace PumpItUpTrainer.Game.Notes
 
             for (int i = 1; i < noteCount; i++)
             {
-                Note mostRecentNote = generatedNotes.Last();
+                Note currentNote = generatedNotes.Last();
 
                 if (currentFoot == Foot.Left)
                 {
-                    List<Note> candidateNotes = nextNotesCurrentFootLeft[mostRecentNote];
+                    List<Note> candidateNotes = nextNotesCurrentFootLeft[currentNote];
+
+                    banCandidateNotes(generatedNotes, currentFoot, candidateNotes);
+
                     generatedNotes.Add(candidateNotes[random.Next(candidateNotes.Count)]);
                     currentFoot = Foot.Right;
                 }
                 else
                 {
-                    List<Note> candidateNotes = nextNotesCurrentFootRight[mostRecentNote];
+                    List<Note> candidateNotes = nextNotesCurrentFootRight[currentNote];
+
+                    banCandidateNotes(generatedNotes, currentFoot, candidateNotes);
+
                     generatedNotes.Add(candidateNotes[random.Next(candidateNotes.Count)]);
                     currentFoot = Foot.Left;
                 }
             }
 
             return generatedNotes;
+        }
+
+        private static void banCandidateNotes(List<Note> noteSequence, Foot currentFoot, List<Note> candidateNotesToBanFrom)
+        {
+            if (noteSequence.Count <= 1)
+            {
+                return;
+            }
+
+            Note lastNote = noteSequence.Last();
+            Note secondToLastNote = noteSequence[^2];
+
+            if (currentFoot == Foot.Left)
+            {
+                if (lastNote == Note.P1C)
+                {
+                    if (secondToLastNote == Note.P1DL)
+                    {
+                        candidateNotesToBanFrom.Remove(Note.P1UL);
+                    }
+                    else if (secondToLastNote == Note.P1UL)
+                    {
+                        candidateNotesToBanFrom.Remove(Note.P1DL);
+                    }
+                }
+                else if (lastNote == Note.P2C)
+                {
+                    if (secondToLastNote == Note.P2DL)
+                    {
+                        candidateNotesToBanFrom.Remove(Note.P2UL);
+                    }
+                    else if (secondToLastNote == Note.P2UL)
+                    {
+                        candidateNotesToBanFrom.Remove(Note.P2DL);
+                    }
+                }
+            }
+            else if (currentFoot == Foot.Right)
+            {
+                if (lastNote == Note.P1C)
+                {
+                    if (secondToLastNote == Note.P1DR)
+                    {
+                        candidateNotesToBanFrom.Remove(Note.P1UR);
+                    }
+                    else if (secondToLastNote == Note.P1UR)
+                    {
+                        candidateNotesToBanFrom.Remove(Note.P1DR);
+                    }
+                }
+                else if (lastNote == Note.P2C)
+                {
+                    if (secondToLastNote == Note.P2DR)
+                    {
+                        candidateNotesToBanFrom.Remove(Note.P2UR);
+                    }
+                    else if (secondToLastNote == Note.P2UR)
+                    {
+                        candidateNotesToBanFrom.Remove(Note.P2DR);
+                    }
+                }
+            }
         }
     }
 }
